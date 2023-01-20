1aee4654a9b4fbd77242802cf2b98fedb79ec7f7
Anders <andersnorgreen@gmail.com>
2023-01-20 09:36:07 +0100

    Added JSON file with list of pickable words and rewired game to accept data from that JSON file

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using static System.Console;
 
 namespace H1GPDag3
 {
     class Program
     {
-        static Random random = new Random();
+        // Creates a filepath for the pool of words the system can pick from, variables to use throughout the program
+        // and brings in Random to help with picking the word to be guessed
 
-        static string[][] words = new string[][]
-        {
-            new string[] {"BEN", "KOP", "API"},
-            new string[] {"STOL", "FISK", "HØNE"},
-            new string[] {"TRIST", "TRIST", "TRIST"},
-        };
+        readonly static string path = @"C:\Users\pc\Desktop\TEC\Grundlæggende programmering\H1GPDag3\";
+        readonly static string fileName = "words.json";
+        static Random random = new Random();
         
-        static string secretWord;
-        static List<string> hiddenWord = new List<string>();
-        static List<string> guessedLetters = new List<string>();
-        static int numberOfGuesses;
-        static int wrongGuesses;
+        static string _secretWord;
+        static List<string> _hiddenWord = new List<string>();
+        static List<string> _guessedLetters = new List<string>();
+        static int _numberOfGuesses;
+        static int _wrongGuesses;
 
         static void Main(string[] args)
         {
@@ -30,9 +31,10 @@ namespace H1GPDag3
 
         static void GameStart()
         {
-            secretWord = "";
-            hiddenWord.Clear();
-            wrongGuesses = 0;
+            _secretWord = "";
+            _hiddenWord.Clear();
+            _wrongGuesses = 5;
+            _guessedLetters.Clear();
 
             Setup();
             DrawGameScreen();
@@ -40,7 +42,7 @@ namespace H1GPDag3
 
         static void DrawHiddenWord()
         {
-            foreach (var b in hiddenWord)
+            foreach (var b in _hiddenWord)
             {
                 Write(b);
             }
@@ -52,18 +54,18 @@ namespace H1GPDag3
             {
                 Clear();
                 HangManTitle();
-                DrawScaffold(wrongGuesses);
+                DrawScaffold(_wrongGuesses);
 
                 DrawHiddenWord();
                 DrawGuessedLetters();
-                WriteLine(Environment.NewLine + $"Du har nu { numberOfGuesses } gæt til at finde det hemmelige ord. Gætter du forkert fem gange eller løber tør for gæt, har du tabt!");
+                WriteLine(Environment.NewLine + $"Du har nu { _numberOfGuesses } gæt til at finde det hemmelige ord. Gætter du forkert { _wrongGuesses } gange eller løber du tør for gæt, har du tabt!");
                 UpdateHiddenWord(HandleGuess());
             }
         }
 
         static string HandleGuess()
         {
-            if (string.Join("", hiddenWord.ToArray()) == secretWord)
+            if (string.Join("", _hiddenWord.ToArray()) == _secretWord)
                 GameWon();
 
             string guess;
@@ -72,22 +74,22 @@ namespace H1GPDag3
                 Write("Indtast næste gæt: ");
                 guess = ReadLine().ToUpper();
 
-                if(guessedLetters.Contains(guess))
+                if(_guessedLetters.Contains(guess))
                     WriteLine("Du har allerede gættet på dette bogstav!");
             }
-            while (guessedLetters.Contains(guess));
+            while (_guessedLetters.Contains(guess));
 
-            numberOfGuesses--;
-            if (numberOfGuesses == 0)
+            _numberOfGuesses--;
+            if (_numberOfGuesses == 0)
                 GameOver();
 
             if (guess.Length == 1)
             {
-                WriteLine(Environment.NewLine + $"Du har nu { numberOfGuesses } gæt tilbage!" + Environment.NewLine);
+                WriteLine(Environment.NewLine + $"Du har nu { _numberOfGuesses } gæt tilbage!" + Environment.NewLine);
 
                 return guess;
             }
-            else if (guess.Length == hiddenWord.Count() && guess == secretWord)
+            else if (guess.Length == _hiddenWord.Count() && guess == _secretWord)
             {
                 GameWon();
             }
@@ -102,26 +104,29 @@ namespace H1GPDag3
                 UpdateHiddenWord(HandleGuess());
             }
 
-            if (secretWord.Contains(guess))
+            if (_secretWord.Contains(guess))
             {
-                for (int i = 0; i < hiddenWord.Count; i++)
+                for (int i = 0; i < _hiddenWord.Count; i++)
                 {
-                    if (secretWord[i] == guess[0])
-                        hiddenWord[i] = guess;
+                    if (_secretWord[i] == guess[0])
+                        _hiddenWord[i] = guess;
                 }
             }
             else
-                wrongGuesses++;
+                _wrongGuesses--;
 
+            if (_wrongGuesses == 0)
+                GameOver();
+            
             if(guess.Length == 1)
-                guessedLetters.Add(guess);
+                _guessedLetters.Add(guess);
         }
-        
+
         static void DrawGuessedLetters()
         {
             WriteLine(Environment.NewLine);
 
-            foreach (string c in guessedLetters)
+            foreach (string c in _guessedLetters)
             {
                 Write(c);
             }
@@ -141,9 +146,15 @@ namespace H1GPDag3
         {
             Clear();
             HangManTitle();
+            if (_wrongGuesses == 0)
+            {
+                DrawScaffold(0);
+                WriteLine("Du har gættet forkert for mange gange, manden er hængt!");
+            }
+            else
+                WriteLine("Beklager, du har ikke flere gæt tilbage. Du tabte!");
 
-            WriteLine("Beklager, du har ikke flere gæt tilbage. Du tabte!");
-            WriteLine("Tryk en tast for at spille igen");
+            WriteLine(Environment.NewLine + "Tryk en tast for at spille igen");
             ReadKey();
             Clear();
             GameStart();
@@ -158,21 +169,54 @@ namespace H1GPDag3
             WriteLine("3. Fem bogstaver");
 
             Write(Environment.NewLine + "Indtast venligst den ønskede sværhedsgrad (længde på det hemmelige ord): ");
-            Int32.TryParse(ReadLine(), out int choice);
 
-            secretWord = words[choice - 1][random.Next(3)];
+            int choice;
 
-            for (int i = 0; i < secretWord.Length; i++)
+            while (!Int32.TryParse(ReadLine(), out choice) || choice < 1 || choice > 3)
             {
-                hiddenWord.Add("_");
+                Write("Du skal vælge et tal fra menuen, prøv igen: ");
             }
 
-            numberOfGuesses = hiddenWord.Count + 3;
+            SecretWordsModel secretWordsModel = GetSecretWords<SecretWordsModel>();
+            int randomWordPick = random.Next(3);
+
+            switch (choice)
+            {
+                case 1:
+                    _secretWord = secretWordsModel.ThreeLetterWords[randomWordPick];
+                    break;
+                case 2:
+                    _secretWord = secretWordsModel.FourLetterWords[randomWordPick];
+                    break;
+                case 3:
+                    _secretWord = secretWordsModel.FiveLetterWords[randomWordPick];
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < _secretWord.Length; i++)
+            {
+                _hiddenWord.Add("_");
+            }
+
+            _numberOfGuesses = _hiddenWord.Count + 3;
+        }
+
+        static T? GetSecretWords<T>()
+        {
+            if (!Directory.Exists(path)) return default;
+            else if (!File.Exists(path + fileName)) return default;
+            
+            string secretWordsJson = File.ReadAllText(path + fileName);
+            T? obj = JsonSerializer.Deserialize<T?>(secretWordsJson);
+
+            return obj;
         }
 
         static void DrawScaffold(int wrongGuesses)
         {
-            if (wrongGuesses == 0)
+            if (wrongGuesses == 5)
             {
                 WriteLine(@"
             _______
@@ -184,24 +228,12 @@ namespace H1GPDag3
         ___/|\___
         ");
             }
-            else if (wrongGuesses == 1)
+            else if (wrongGuesses == 4)
             {
                 WriteLine(@"
             _______
             |/     |
             |
-            |
-            |
-            |
-        ___/|\___
-        ");
-            }
-            else if (wrongGuesses == 2)
-            {
-                WriteLine(@"
-            _______
-            |/     |
-            |     (_)
             |
             |
             |
@@ -214,13 +246,25 @@ namespace H1GPDag3
             _______
             |/     |
             |     (_)
+            |
+            |
+            |
+        ___/|\___
+        ");
+            }
+            else if (wrongGuesses == 2)
+            {
+                WriteLine(@"
+            _______
+            |/     |
+            |     (_)
             |     \|/
             |
             |
         ___/|\___
         ");
             }
-            else if (wrongGuesses == 4)
+            else if (wrongGuesses == 1)
             {
                 WriteLine(@"
             _______
@@ -232,7 +276,7 @@ namespace H1GPDag3
         ___/|\___
         ");
             }
-            else if (wrongGuesses == 5)
+            else if (wrongGuesses == 0)
             {
                 Clear();
                 HangManTitle();
@@ -246,7 +290,6 @@ namespace H1GPDag3
             |     / \
         ___/|\___
         ");
-                GameOver();
             }
         }
 
---
+++ SecretWordsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1GPDag3
+{
+    public class SecretWordsModel
+    {
+        public string[] ThreeLetterWords { get; set; }
+        public string[] FourLetterWords { get; set; }
+        public string[] FiveLetterWords { get; set; }
+    }
+}
